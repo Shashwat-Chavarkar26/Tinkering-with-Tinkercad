c246b35635bcd4af42ae1795ebdd50004f31b7c4
Shashwat-Chavarkar26 <shashwat.chavarkar@gmail.com>
2025-07-21 23:46:59 +0530

    Add files via upload

---
+++ diwali_lights1.ino
@@ -0,0 +1,86 @@
+// C++ code
+//
+int green = 3;
+int red = 4;
+int blue = 5;
+int orange4 = 6;
+int yellow5 = 7;
+int green6 = 8;
+int RED7 = 9;
+int blue8 = 10;
+int or9 = 11;
+int ye10 = 12;
+int gre11 = 13;
+  
+void setup()
+{
+  pinMode( 2 , OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(green, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(green, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+{
+   digitalWrite(red, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(red, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+}
+  {
+     digitalWrite(blue, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(blue, LOW);
+  delay(100); //
+}
+  {
+     digitalWrite(orange4, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(orange4, LOW);
+  delay(100); //
+  }
+  {
+     digitalWrite(yellow5, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(yellow5, LOW);
+  delay(100); //
+  }
+  {
+     digitalWrite(green6, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(green6, LOW);
+  delay(100); //
+  }
+  {
+     digitalWrite(RED7, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(RED7, LOW);
+  delay(100); //
+  }
+  {
+    digitalWrite(blue8, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(blue8, LOW);
+  delay(100); //
+  }
+  {
+  digitalWrite(or9, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(or9, LOW);
+  delay(100); //
+  }
+  {
+    digitalWrite(ye10, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(ye10, LOW);
+  delay(100); //
+  }
+  {
+    digitalWrite(gre11, HIGH);
+  delay(50); // Wait for 1000 millisecond(s)
+  digitalWrite(gre11, LOW);
+  delay(100); //
+  }
+}
